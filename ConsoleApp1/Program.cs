4a21e059fe6de74b59768c1c6571df49e5045ce2
Ilya <ieelokhov@edu.hse.ru>
2025-05-05 21:18:34 +0500

    3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,14 @@
 {
     internal class Program
     {
+        void sum(int n, int m)
+        {
+            Console.WriteLine(n+m);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine(2+2);
+            Program a = new Program();
+            a.sum(1, 2);
         }
     }
 }
